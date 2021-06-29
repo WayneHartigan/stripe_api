bd29861c343c78bbc0251e3090b607da461abc15
Wayne Hartigan <hartiganwayne@gmail.com>
2021-06-29 22:49:34 +0100

    Charge objects

--- stripe_tutorial.py
+++ stripe_tutorial.py
@@ -3,17 +3,26 @@ import stripe
 
 stripe.api_key = os.environ['STRIPE_SECRET_KEY']
 
-balance = stripe.Balance.retrieve()
+# balance = stripe.Balance.retrieve()
 # print(balance)
 
-# resp = stripe.Charge.create(
-#   amount=2000,
-#   currency="eur",
-#   source="tok_mastercard",
-#   description="My First Test Charge (created for API docs)",
-# )
+resp = stripe.Charge.create(
+  amount=2000,
+  currency="eur",
+  source="tok_mastercard",
+  description="My First Test Charge (created for API docs)",
+)
 # print(resp)
 
-balance = stripe.Balance.retrieve()
-print(balance)
+# balance = stripe.Balance.retrieve()
+# print(balance)
 
+stripe.Charge.modify(
+  resp['id'],
+  metadata={"order_id": "1958"},
+)
+
+get_resp = stripe.Charge.retrieve(resp['id'])
+# print(get_resp)
+print(stripe.Charge.list())
+# stripe.Charge.capture(resp['id'])
